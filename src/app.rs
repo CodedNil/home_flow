d62ac1be343dd70b5aa52ff7557e4444915c67ff
Dan Lock <codenil@proton.me>
2024-01-25 20:36:21 +0000

    Flip y and render mesh fully

--- src/app.rs
+++ src/app.rs
@@ -42,61 +42,28 @@ impl HomeFlow {
         Self::default()
     }
 
-    fn pixels_to_world(&self, canvas_center: Pos2, pixels: Pos2) -> Vec2 {
-        (pixels - canvas_center) / self.zoom - self.translation
+    fn pixels_to_world(&self, canvas_center: Pos2, x: f32, y: f32) -> Pos2 {
+        Pos2::new(x - canvas_center.x, canvas_center.y - y) / self.zoom - self.translation
     }
 
-    fn pixels_to_world_x(&self, canvas_center: Pos2, pixels: f32) -> f32 {
-        (pixels - canvas_center.x) / self.zoom - self.translation.x
-    }
-
-    fn pixels_to_world_y(&self, canvas_center: Pos2, pixels: f32) -> f32 {
-        (pixels - canvas_center.y) / self.zoom - self.translation.y
-    }
-
-    fn world_to_pixels(&self, canvas_center: Pos2, world: Pos2) -> Pos2 {
-        (world + self.translation) * self.zoom + Vec2::new(canvas_center.x, canvas_center.y)
-    }
-
-    fn world_to_pixels_x(&self, canvas_center: Pos2, world: f32) -> f32 {
-        (world + self.translation.x) * self.zoom + canvas_center.x
-    }
-
-    fn world_to_pixels_y(&self, canvas_center: Pos2, world: f32) -> f32 {
-        (world + self.translation.y) * self.zoom + canvas_center.y
+    fn world_to_pixels(&self, canvas_center: Pos2, x: f32, y: f32) -> Pos2 {
+        Pos2::new(x + self.translation.x, self.translation.y - y) * self.zoom + Vec2::new(canvas_center.x, canvas_center.y)
     }
 
     fn render_grid(&self, painter: &Painter, visible_rect: &Rect, canvas_center: Pos2) {
+        let grid_interval = 2.0_f32.powf((160.0 / self.zoom).abs().log2().round());
         let grid_intervals = [
-            (
-                if self.zoom <= 40.0 {
-                    4.0
-                } else if self.zoom <= 80.0 {
-                    2.0
-                } else {
-                    1.0
-                },
-                Stroke::new(1.5, Color32::from_rgb(85, 85, 100)),
-            ),
-            (
-                if self.zoom <= 40.0 {
-                    1.0
-                } else if self.zoom <= 80.0 {
-                    0.5
-                } else {
-                    0.25
-                },
-                Stroke::new(1.5, Color32::from_rgb(55, 55, 70)),
-            ),
+            (grid_interval, Stroke::new(1.5, Color32::from_rgb(85, 85, 100))),
+            (grid_interval / 4.0, Stroke::new(1.5, Color32::from_rgb(55, 55, 70))),
         ];
 
-        let (top_edge_world, bottom_edge_world) = (
-            self.pixels_to_world_y(canvas_center, visible_rect.top()),
-            self.pixels_to_world_y(canvas_center, visible_rect.bottom()),
+        let (bottom_edge_world, top_edge_world) = (
+            -self.pixels_to_world(canvas_center, 0.0, visible_rect.top()).y,
+            -self.pixels_to_world(canvas_center, 0.0, visible_rect.bottom()).y,
         );
         let (left_edge_world, right_edge_world) = (
-            self.pixels_to_world_x(canvas_center, visible_rect.left()),
-            self.pixels_to_world_x(canvas_center, visible_rect.right()),
+            self.pixels_to_world(canvas_center, visible_rect.left(), 0.0).x,
+            self.pixels_to_world(canvas_center, visible_rect.right(), 0.0).x,
         );
 
         let mut rendered_vertical = HashSet::new();
@@ -105,7 +72,7 @@ impl HomeFlow {
         for (grid_interval, stroke) in grid_intervals {
             // Draw vertical grid lines
             for x in ((left_edge_world / grid_interval).ceil() as i32)..=((right_edge_world / grid_interval).floor() as i32) {
-                let grid_line_pixel = self.world_to_pixels_x(canvas_center, x as f32 * grid_interval);
+                let grid_line_pixel = self.world_to_pixels(canvas_center, x as f32 * grid_interval, 0.0).x;
                 let grid_line_pixel_int = (grid_line_pixel * 100.0).round() as i32;
                 if rendered_vertical.contains(&grid_line_pixel_int) {
                     continue;
@@ -119,8 +86,8 @@ impl HomeFlow {
             }
 
             // Draw horizontal grid lines
-            for y in ((top_edge_world / grid_interval).ceil() as i32)..=((bottom_edge_world / grid_interval).floor() as i32) {
-                let grid_line_pixel = self.world_to_pixels_y(canvas_center, y as f32 * grid_interval);
+            for y in ((bottom_edge_world / grid_interval).ceil() as i32)..=((top_edge_world / grid_interval).floor() as i32) {
+                let grid_line_pixel = self.world_to_pixels(canvas_center, 0.0, y as f32 * grid_interval).y;
                 let grid_line_pixel_int = (grid_line_pixel * 100.0).round() as i32;
                 if rendered_horizontal.contains(&grid_line_pixel_int) {
                     continue;
@@ -141,13 +108,13 @@ impl HomeFlow {
     fn render_box(&self, painter: &Painter, canvas_center: Pos2, pos: Pos2, size: Vec2, color: Color32) {
         let box_size = Vec2::new(size.x * self.zoom, size.y * self.zoom);
         painter.rect_filled(
-            Rect::from_center_size(self.world_to_pixels(canvas_center, pos), box_size),
+            Rect::from_center_size(self.world_to_pixels(canvas_center, pos.x, pos.y), box_size),
             0.0,
             color,
         );
     }
 
-    fn render_mesh(&self, painter: &Painter, canvas_center: Pos2, vertices: Vec<Pos2>, color: Color32) {
+    fn render_mesh(&self, painter: &Painter, canvas_center: Pos2, vertices: Vec<Vertex>) {
         let mut indices = Vec::new();
         for i in 0..vertices.len() {
             indices.push(i as u32);
@@ -155,9 +122,8 @@ impl HomeFlow {
         let mut vertices_pixels = Vec::new();
         for vertex in vertices {
             vertices_pixels.push(Vertex {
-                pos: self.world_to_pixels(canvas_center, vertex),
-                color,
-                ..Default::default()
+                pos: self.world_to_pixels(canvas_center, vertex.pos.x, vertex.pos.y),
+                ..vertex
             });
         }
 
@@ -191,7 +157,7 @@ impl eframe::App for HomeFlow {
                 ..Default::default()
             })
             .show(ctx, |ui| {
-                // ui.ctx().request_repaint();
+                ui.ctx().request_repaint();
                 self.time += ui.input(|i| i.unstable_dt) as f64;
 
                 let (response, painter) = ui.allocate_painter(ui.available_size(), Sense::drag());
@@ -205,9 +171,9 @@ impl eframe::App for HomeFlow {
                 if scroll_delta != Vec2::ZERO {
                     let zoom_amount = (scroll_delta.y.signum() * 15.0) * (self.zoom / 100.0);
                     if let Some(mouse_pos) = ui.input(|i| i.pointer.latest_pos()) {
-                        let mouse_world_before_zoom = self.pixels_to_world(canvas_center, mouse_pos);
+                        let mouse_world_before_zoom = self.pixels_to_world(canvas_center, mouse_pos.x, mouse_pos.y);
                         self.zoom = (self.zoom + zoom_amount).clamp(20.0, 300.0);
-                        let mouse_world_after_zoom = self.pixels_to_world(canvas_center, mouse_pos);
+                        let mouse_world_after_zoom = self.pixels_to_world(canvas_center, mouse_pos.x, mouse_pos.y);
                         self.translation += mouse_world_after_zoom - mouse_world_before_zoom;
                     } else {
                         self.zoom = (self.zoom + zoom_amount).clamp(20.0, 300.0);
@@ -242,22 +208,71 @@ impl eframe::App for HomeFlow {
                 );
 
                 self.render_grid(&painter, &response.rect, canvas_center);
+                self.render_box(
+                    &painter,
+                    canvas_center,
+                    Pos2::new(3.0, 0.0),
+                    Vec2::new(1.0, 1.0),
+                    Color32::from_rgb(255, 0, 0),
+                );
+                self.render_box(
+                    &painter,
+                    canvas_center,
+                    Pos2::new(-3.0, 0.0),
+                    Vec2::new(1.0, 1.0),
+                    Color32::from_rgb(0, 255, 0),
+                );
+                self.render_box(
+                    &painter,
+                    canvas_center,
+                    Pos2::new(0.0, 3.0),
+                    Vec2::new(1.0, 1.0),
+                    Color32::from_rgb(0, 0, 255),
+                );
+                self.render_box(
+                    &painter,
+                    canvas_center,
+                    Pos2::new(0.0, -3.0),
+                    Vec2::new(1.0, 1.0),
+                    Color32::from_rgb(255, 255, 0),
+                );
 
                 for room in &self.layout.rooms {
                     let pos = room.pos;
                     let size = Vec2::new(room.size.x, room.size.y);
                     let size_half = size / 2.0;
-                    let vertices = vec![
-                        Pos2::new(pos.x - size_half.x, pos.y + size_half.y), // Bottom-left
-                        Pos2::new(pos.x + size_half.x, pos.y + size_half.y), // Bottom-right
-                        Pos2::new(pos.x + size_half.x, pos.y - size_half.y), // Top-right
-                        Pos2::new(pos.x - size_half.x, pos.y - size_half.y), // Top-left
-                    ];
-                    self.render_mesh(&painter, canvas_center, vertices, Color32::from_rgb(255, 0, 0));
+
+                    // Define vertices for the rectangle
+                    let bottom_left = Vertex {
+                        pos: Pos2::new(pos.x - size_half.x, pos.y - size_half.y),
+                        uv: Pos2::new(0.0, 0.0),
+                        color: Color32::from_rgb(255, 0, 0),
+                    };
+                    let bottom_right = Vertex {
+                        pos: Pos2::new(pos.x + size_half.x, pos.y - size_half.y),
+                        uv: Pos2::new(0.0, 0.0),
+                        color: Color32::from_rgb(0, 255, 0),
+                    };
+                    let top_right = Vertex {
+                        pos: Pos2::new(pos.x + size_half.x, pos.y + size_half.y),
+                        uv: Pos2::new(0.0, 0.0),
+                        color: Color32::from_rgb(0, 0, 255),
+                    };
+                    let top_left = Vertex {
+                        pos: Pos2::new(pos.x - size_half.x, pos.y + size_half.y),
+                        uv: Pos2::new(0.0, 0.0),
+                        color: Color32::from_rgb(255, 255, 0),
+                    };
+
+                    self.render_mesh(
+                        &painter,
+                        canvas_center,
+                        vec![bottom_left, bottom_right, top_right, top_right, top_left, bottom_left],
+                    );
                 }
 
-                let plot_location = self.world_to_pixels(canvas_center, Pos2::new(3.0, 2.0));
-                let plot_end_location = self.world_to_pixels(canvas_center, Pos2::new(8.0, 6.0));
+                let plot_location = self.world_to_pixels(canvas_center, 3.0, -2.0);
+                let plot_end_location = self.world_to_pixels(canvas_center, 8.0, -6.0);
                 Window::new("Plot Window")
                     .fixed_pos(plot_location)
                     .fixed_size(plot_end_location - plot_location)
