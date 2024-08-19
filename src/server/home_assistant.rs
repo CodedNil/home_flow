1d74a8b89c3a4d1af3b53f2bf3ca03b40aff40fe
Dan <codednil@gmail.com>
2024-08-19 15:36:39 +0100

    Optimised serverside calculations

--- src/server/home_assistant.rs
+++ src/server/home_assistant.rs
@@ -9,7 +9,7 @@ use crate::common::utils::rotate_point_i32;
 use anyhow::{anyhow, Result};
 use axum::body::Bytes;
 use axum::{http::StatusCode, response::IntoResponse};
-use glam::{dvec2 as vec2, dvec3 as vec3, DMat3, DVec2 as Vec2};
+use glam::{dvec2 as vec2, DMat3, DVec2 as Vec2};
 use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
 use serde::Deserialize;
 use serde_json::json;
@@ -19,7 +19,7 @@ use std::sync::LazyLock;
 use tokio::sync::Mutex;
 use tokio::time::Instant;
 
-static LOOP_INTERVAL_MS: u64 = 500;
+static LOOP_INTERVAL_MS: u64 = 200;
 static CALIBRATION_DURATION: f64 = 30.0;
 
 fn get_env_variable(key: &str) -> String {
@@ -38,6 +38,9 @@ static HA_STATE: LazyLock<Mutex<Option<HAState>>> = LazyLock::new(|| Mutex::new(
 
 /// Every X seconds, get the states from Home Assistant
 pub async fn server_loop() {
+    let client = reqwest::Client::new();
+    let url = format!("{}/api/states", get_env_variable("HASS_URL"));
+    let token = format!("Bearer {}", get_env_variable("HASS_TOKEN"));
     loop {
         // Get list of sensors to fetch
         let layout = HOME.lock().await;
@@ -55,7 +58,9 @@ pub async fn server_loop() {
         }
         drop(layout);
 
-        let states = get_states_impl(sensors).await.unwrap();
+        let states = get_states_impl(&client, &url, &token, sensors)
+            .await
+            .unwrap();
         *HA_STATE.lock().await = Some(states);
         tokio::time::sleep(tokio::time::Duration::from_millis(LOOP_INTERVAL_MS)).await;
     }
@@ -124,65 +129,66 @@ static LAST_OCCUPANCY: LazyLock<Mutex<HashMap<String, (bool, u8)>>> =
 static PRESENCE_CALIBRATION: LazyLock<Mutex<Option<(Instant, Vec<Vec2>)>>> =
     LazyLock::new(|| Mutex::new(None));
 
-async fn get_states_impl(sensors: Vec<String>) -> Result<HAState> {
-    let client = reqwest::Client::new();
+async fn get_states_impl(
+    client: &reqwest::Client,
+    url: &str,
+    token: &str,
+    target_sensors: Vec<String>,
+) -> Result<HAState> {
     let states_raw = client
-        .get(&format!("{}/api/states", get_env_variable("HASS_URL")))
-        .header(
-            AUTHORIZATION,
-            format!("Bearer {}", get_env_variable("HASS_TOKEN")),
-        )
+        .get(url)
+        .header(AUTHORIZATION, token)
         .header(CONTENT_TYPE, "application/json")
         .send()
         .await?
         .json::<Vec<HassState>>()
         .await?;
 
-    let mut light_states = Vec::new();
-    let mut sensor_states = Vec::new();
-    for state_raw in &states_raw {
-        let entity_id = state_raw.entity_id.split('.').nth(1).unwrap();
-        if state_raw.entity_id.starts_with("light.") {
-            let mut state_value = match state_raw.state.as_str() {
-                "on" => 255,
-                _ => 0,
-            };
-            // Check if attributes has brightness
-            if let Some(brightness) = state_raw.attributes.get("brightness") {
-                if let Some(brightness) = brightness.as_u64() {
-                    state_value = brightness as u8;
-                }
-            }
+    let mut lights = Vec::new();
+    let mut sensors = Vec::new();
 
-            let state = LightPacket {
-                entity_id: entity_id.to_string(),
-                state: state_value,
-            };
-            light_states.push(state);
-        }
-        if state_raw.entity_id.starts_with("sensor.") && sensors.contains(&entity_id.to_string()) {
-            let state = SensorPacket {
-                entity_id: entity_id.to_string(),
-                state: state_raw.state.clone(),
-            };
-            sensor_states.push(state);
+    for state_raw in &states_raw {
+        if let Some((domain, entity_id)) = state_raw.entity_id.split_once('.') {
+            match domain {
+                "light" => lights.push(LightPacket {
+                    entity_id: entity_id.to_string(),
+                    state: state_raw
+                        .attributes
+                        .get("brightness")
+                        .and_then(serde_json::Value::as_u64)
+                        .map_or_else(
+                            || if state_raw.state == "on" { 255 } else { 0 },
+                            |b| b as u8,
+                        ),
+                }),
+                "sensor" if target_sensors.contains(&entity_id.to_string()) => {
+                    sensors.push(SensorPacket {
+                        entity_id: entity_id.to_string(),
+                        state: state_raw.state.clone(),
+                    });
+                }
+                _ => {}
+            }
         }
     }
 
     // Begin calibration if needed
-    let calibration = PRESENCE_CALIBRATION.lock().await.clone();
-    let presence_calibration = states_raw
-        .iter()
-        .find(|state| state.entity_id == "input_boolean.presence_calibration")
-        .map_or_else(|| false, |state| state.state == "on");
-    if calibration.is_none() && presence_calibration {
-        *PRESENCE_CALIBRATION.lock().await = Some((Instant::now(), Vec::new()));
-        log::info!("Presence calibration started");
+    let mut calibration_lock = PRESENCE_CALIBRATION.lock().await;
+    let presence_calibration = states_raw.iter().any(|state| {
+        state.entity_id == "input_boolean.presence_calibration" && state.state == "on"
+    });
+    match (&*calibration_lock, presence_calibration) {
+        (None, true) => {
+            *calibration_lock = Some((Instant::now(), Vec::new()));
+            log::info!("Presence calibration started");
+        }
+        (Some(_), false) => {
+            *calibration_lock = None;
+        }
+        _ => {}
     }
-    // If calibration becomes false, end it
-    if calibration.is_some() && !presence_calibration {
-        *PRESENCE_CALIBRATION.lock().await = None;
-    }
+    let is_calibrating = calibration_lock.is_some();
+    drop(calibration_lock);
 
     let layout = HOME.lock().await.clone();
 
@@ -194,52 +200,42 @@ async fn get_states_impl(sensors: Vec<String>) -> Result<HAState> {
                 == FurnitureType::Electronic(ElectronicType::UltimateSensorMini)
             {
                 // Read targets from the sensor
-                let mut targets: Vec<Vec2> = Vec::new();
-                for i in 1..=5 {
-                    for entity_id in &furniture.misc_sensors {
-                        if entity_id.ends_with(&format!("target_{i}_x"))
-                            || entity_id.ends_with(&format!("target_{i}_y"))
-                        {
-                            let is_x = entity_id.ends_with("_x");
-                            let value = states_raw
-                                .iter()
-                                .find(|state| state.entity_id == format!("sensor.{entity_id}"))
-                                .and_then(|state| state.state.parse::<f64>().ok())
-                                .unwrap_or(0.0);
+                let targets = (1..=5)
+                    .map(|i| {
+                        let mut x = 0.0;
+                        let mut y = 0.0;
 
-                            // If target x already exists, override the x or y, else add a new target
-                            if targets.len() >= i {
-                                let target = *targets.get(i - 1).unwrap();
-                                if is_x {
-                                    targets[i - 1] = vec2(value, target.y);
-                                } else {
-                                    targets[i - 1] = vec2(target.x, value);
+                        for id in &furniture.misc_sensors {
+                            if let Some(value) = states_raw
+                                .iter()
+                                .find(|state| state.entity_id == format!("sensor.{id}"))
+                                .and_then(|state| state.state.parse::<f64>().ok())
+                            {
+                                if id.ends_with(&format!("target_{i}_x")) {
+                                    x = value;
+                                } else if id.ends_with(&format!("target_{i}_y")) {
+                                    y = value;
                                 }
-                            } else {
-                                targets.push(if is_x {
-                                    vec2(value, 0.0)
-                                } else {
-                                    vec2(0.0, value)
-                                });
                             }
                         }
-                    }
-                }
 
-                // Filter out zero targets
-                targets.retain(|&v| v != Vec2::ZERO);
-                if calibration.is_some() {
+                        vec2(x, y)
+                    })
+                    .filter(|&v| v != Vec2::ZERO)
+                    .collect::<Vec<_>>();
+
+                if is_calibrating {
                     presence_points_raw.extend(targets.clone());
                 }
 
                 // Check if sensor has reference points
                 if let (
-                    Some(DataPoint::Vec2(reference_point1)),
-                    Some(DataPoint::Vec2(reference_world1)),
-                    Some(DataPoint::Vec2(reference_point2)),
-                    Some(DataPoint::Vec2(reference_world2)),
-                    Some(DataPoint::Vec2(reference_point3)),
-                    Some(DataPoint::Vec2(reference_world3)),
+                    Some(DataPoint::Vec2(rp1)),
+                    Some(DataPoint::Vec2(rw1)),
+                    Some(DataPoint::Vec2(rp2)),
+                    Some(DataPoint::Vec2(rw2)),
+                    Some(DataPoint::Vec2(rp3)),
+                    Some(DataPoint::Vec2(rw3)),
                 ) = (
                     furniture.misc_data.get("calib_point1"),
                     furniture.misc_data.get("calib_world1"),
@@ -248,51 +244,72 @@ async fn get_states_impl(sensors: Vec<String>) -> Result<HAState> {
                     furniture.misc_data.get("calib_point3"),
                     furniture.misc_data.get("calib_world3"),
                 ) {
-                    let (a, b, c, d, tx, ty) = solve_affine_transformation(
-                        reference_point1,
-                        reference_point2,
-                        reference_point3,
-                        reference_world1,
-                        reference_world2,
-                        reference_world3,
-                    );
-
-                    // Transform live sensor point to real-world position
-                    for target in &targets {
-                        let real_world_pos = vec2(
+                    // Solve affine transformation matrix
+                    let (a, b, c, d, tx, ty) = {
+                        let sensor_matrix =
+                            DMat3::from_cols(rp1.extend(1.0), rp2.extend(1.0), rp3.extend(1.0));
+                        let world_matrix =
+                            DMat3::from_cols(rw1.extend(1.0), rw2.extend(1.0), rw3.extend(1.0));
+                        let transformation_matrix = world_matrix * sensor_matrix.inverse();
+                        (
+                            transformation_matrix.x_axis.x,
+                            transformation_matrix.y_axis.x,
+                            transformation_matrix.x_axis.y,
+                            transformation_matrix.y_axis.y,
+                            transformation_matrix.z_axis.x,
+                            transformation_matrix.z_axis.y,
+                        )
+                    };
+                    presence_points.extend(targets.iter().map(|target| {
+                        vec2(
                             a * target.x + b * target.y + tx,
                             c * target.x + d * target.y + ty,
-                        );
-                        presence_points.push(real_world_pos);
-                    }
+                        )
+                    }));
                 } else {
-                    // Transform live sensor point to real-world position
-                    for target in &targets {
-                        let real_world_pos = room.pos
+                    presence_points.extend(targets.iter().map(|target| {
+                        room.pos
                             + furniture.pos
-                            + rotate_point_i32(*target / 1000.0, -furniture.rotation);
-                        presence_points.push(real_world_pos);
-                    }
+                            + rotate_point_i32(*target / 1000.0, -furniture.rotation)
+                    }));
                 };
             }
         }
     }
     // Merge close points
-    presence_points = merge_close_points(&presence_points, 0.1);
+    presence_points = {
+        let mut points = presence_points.clone();
+        let mut merged_points = Vec::new();
+
+        while let Some(point) = points.pop() {
+            let mut cluster = vec![point];
+
+            points.retain(|&other_point| {
+                if (point - other_point).length() <= 0.1 {
+                    cluster.push(other_point);
+                    false
+                } else {
+                    true
+                }
+            });
+
+            let centroid = cluster.iter().copied().sum::<Vec2>() / cluster.len() as f64;
+            merged_points.push(centroid);
+        }
+
+        merged_points
+    };
 
     // If calibrating, add raw points to data if they are not already present
     {
         let mut calibration = PRESENCE_CALIBRATION.lock().await;
-        let num_calib_points = calibration.as_ref().map_or(0, |(_, points)| points.len());
         if let Some((start_time, calibration_points)) = calibration.as_mut() {
             if start_time.elapsed().as_secs_f64() > CALIBRATION_DURATION {
-                // Get average of all points
-                let mut sum = Vec2::ZERO;
-                for point in calibration_points {
-                    sum += *point;
-                }
-                let average = (sum / num_calib_points as f64).round();
+                let average = (calibration_points.iter().copied().sum::<Vec2>()
+                    / calibration_points.len() as f64)
+                    .round();
                 log::info!("Calibration ended, average point: {:?}", average);
+
                 // Set input_boolean.presence_calibration to false and input_text.presence_calibration_output to the average point
                 post_actions_impl(vec![
                     PostActionsData {
@@ -315,6 +332,7 @@ async fn get_states_impl(sensors: Vec<String>) -> Result<HAState> {
                 ])
                 .await
                 .unwrap();
+
                 *calibration = None;
                 drop(calibration);
             } else {
@@ -323,38 +341,35 @@ async fn get_states_impl(sensors: Vec<String>) -> Result<HAState> {
         }
     }
 
-    // Calculate which zones are occupied
+    // Calculate zone occupancy
     let mut zone_occupancy: HashMap<String, (bool, u8)> = HashMap::new();
     for room in &layout.rooms {
-        let mut occupied = false;
-        let mut targets = 0;
-        for point in &presence_points {
-            if room.contains(*point) {
-                occupied = true;
-                targets += 1;
-            }
-        }
+        let room_occupancy = presence_points
+            .iter()
+            .filter(|&&p| room.contains(p))
+            .count();
+
         zone_occupancy.insert(
             room.name.to_lowercase().replace(' ', "_"),
-            (occupied, targets),
+            (room_occupancy > 0, room_occupancy as u8),
         );
+
         for zone in &room.zones {
-            let mut zone_occupancy_entry = (false, 0);
-            for point in &presence_points {
-                if zone.contains(room.pos, *point) {
-                    zone_occupancy_entry.0 = true;
-                    zone_occupancy_entry.1 += 1;
-                }
-            }
+            let zone_occupancy_count = presence_points
+                .iter()
+                .filter(|&&p| zone.contains(room.pos, p))
+                .count();
+
             zone_occupancy
                 .entry(zone.name.to_lowercase().replace(' ', "_"))
                 .and_modify(|e| {
-                    e.0 |= zone_occupancy_entry.0; // Set occupied to true if any are occupied
-                    e.1 += zone_occupancy_entry.1; // Sum up all targets
+                    e.0 |= zone_occupancy_count > 0;
+                    e.1 += zone_occupancy_count as u8;
                 })
-                .or_insert(zone_occupancy_entry);
+                .or_insert((zone_occupancy_count > 0, zone_occupancy_count as u8));
         }
     }
+
     // Compare to LAST_OCCUPANCY and for differences, post actions
     let mut last_occupancy = LAST_OCCUPANCY.lock().await;
     let mut post_data = Vec::new();
@@ -385,13 +400,14 @@ async fn get_states_impl(sensors: Vec<String>) -> Result<HAState> {
         }
     }
     drop(last_occupancy);
+
     if !post_data.is_empty() {
         post_actions_impl(post_data).await?;
     }
 
     Ok(HAState {
-        lights: light_states,
-        sensors: sensor_states,
+        lights,
+        sensors,
         presence_points,
     })
 }
@@ -449,68 +465,4 @@ async fn post_actions_impl(data: Vec<PostActionsData>) -> Result<()> {
             errors
         ))
     }
-}
-
-fn merge_close_points(points: &[Vec2], radius: f64) -> Vec<Vec2> {
-    let mut merged_points = Vec::new();
-    let mut used = vec![false; points.len()];
-
-    for (i, &point) in points.iter().enumerate() {
-        if used[i] {
-            continue;
-        }
-
-        let mut cluster = Vec::new();
-
-        for (j, &other_point) in points.iter().enumerate().skip(i) {
-            if !used[j] && (point - other_point).length() <= radius {
-                cluster.push(other_point);
-                used[j] = true;
-            }
-        }
-
-        let centroid = cluster.iter().copied().sum::<Vec2>() / cluster.len() as f64;
-        merged_points.push(centroid);
-    }
-
-    merged_points
-}
-
-fn solve_affine_transformation(
-    p1: &Vec2,
-    p2: &Vec2,
-    p3: &Vec2,
-    w1: &Vec2,
-    w2: &Vec2,
-    w3: &Vec2,
-) -> (f64, f64, f64, f64, f64, f64) {
-    // Create matrix for sensor points
-    let sensor_matrix = DMat3::from_cols(
-        vec3(p1.x, p1.y, 1.0),
-        vec3(p2.x, p2.y, 1.0),
-        vec3(p3.x, p3.y, 1.0),
-    );
-
-    // Create matrix for world points
-    let world_matrix = DMat3::from_cols(
-        vec3(w1.x, w1.y, 1.0),
-        vec3(w2.x, w2.y, 1.0),
-        vec3(w3.x, w3.y, 1.0),
-    );
-
-    // Inverse the sensor matrix
-    let sensor_matrix_inv = sensor_matrix.inverse();
-
-    // Calculate the transformation matrix
-    let transformation_matrix = world_matrix * sensor_matrix_inv;
-
-    // Extract transformation parameters
-    let a = transformation_matrix.x_axis.x;
-    let b = transformation_matrix.y_axis.x;
-    let tx = transformation_matrix.z_axis.x;
-    let c = transformation_matrix.x_axis.y;
-    let d = transformation_matrix.y_axis.y;
-    let ty = transformation_matrix.z_axis.y;
-
-    (a, b, c, d, tx, ty)
 }
